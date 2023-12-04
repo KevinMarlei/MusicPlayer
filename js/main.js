68230091d1700aeb0b9f237bace70e58801bd581
Kevin Marlei <kevin_SSDP@hotmail.com>
2023-12-04 18:58:14 +0100

    Adição de listagem de gêneros musicais dinâmicamente

--- js/main.js
+++ js/main.js
@@ -13,24 +13,24 @@ document.querySelector('.btn-play').addEventListener('click', playMusic);
 document.querySelector('.btn-pause').addEventListener('click', pauseMusic);
 volumeControl.addEventListener('input', controlVolume);
 
-volumeControl.addEventListener('wheel', function(event){
+volumeControl.addEventListener('wheel', function (event) {
     event.preventDefault();
     volumeControl.value = parseInt(volumeControl.value) + (event.deltaY > 0 ? -1 : 1);
     volumeControl.value = Math.max(0, Math.min(100, volumeControl.value));
     controlVolume();
 });
 
-document.querySelector('.btn-next').addEventListener('click', ()=>{
+document.querySelector('.btn-next').addEventListener('click', () => {
     indexMusic++;
-    if(indexMusic >= objectMusic.length){
+    if (indexMusic >= objectMusic.length) {
         indexMusic = 0;
     }
     renderMusic(indexMusic);
 });
-document.querySelector('.btn-previous').addEventListener('click', ()=>{
+document.querySelector('.btn-previous').addEventListener('click', () => {
     indexMusic--;
-    if(indexMusic < 0){
-        indexMusic = objectMusic.length -1;
+    if (indexMusic < 0) {
+        indexMusic = objectMusic.length - 1;
     }
     renderMusic(indexMusic);
 });
@@ -43,14 +43,14 @@ function playMusic() {
 }
 
 function pauseMusic() {
-    
+
     music.pause();
 }
 
 function updateBar() {
     let range = document.querySelector('.progre[type="range"]');
     let theCurrentTime = document.querySelector('.start');
-    
+
     let percentage = (music.currentTime / music.duration) * 100;
 
     range.value = percentage;
@@ -61,7 +61,7 @@ function clickBar() {
     let range = document.querySelector('.progre[type="range"]');
     let music = document.querySelector('audio');
 
-    range.addEventListener('input', function() {
+    range.addEventListener('input', function () {
         // Obtém a porcentagem do controle deslizante
         let percentage = range.value;
 
@@ -78,19 +78,19 @@ function clickBar() {
 clickBar();
 
 
-function secondsToMinutes(seconds){
+function secondsToMinutes(seconds) {
     let fieldMinutes = Math.floor(seconds / 60);
     let fieldSeconds = seconds % 60;
-    if(fieldSeconds < 10){
+    if (fieldSeconds < 10) {
         fieldSeconds = '0' + fieldSeconds;
     }
     return `${fieldMinutes}:${fieldSeconds}`;
 }
 
 
-function renderMusic(index){
+function renderMusic(index) {
     music.setAttribute('src', objectMusic[index].src);
-    music.addEventListener('loadeddata', ()=>{
+    music.addEventListener('loadeddata', () => {
         musicName.textContent = objectMusic[index].title;
         artistName.textContent = objectMusic[index].artist;
         image.src = objectMusic[index].img;
@@ -99,48 +99,98 @@ function renderMusic(index){
     playMusic();
 }
 
-function controlVolume(){
+function controlVolume() {
     const highVolumeIcon = document.querySelector('.high-volume');
     const mediumVolumeIcon = document.querySelector('.medium-volume');
     const lowVolumeIcon = document.querySelector('.low-volume');
     const zeroVolumeIcon = document.querySelector('.zero-volume');
-   music.volume = volumeControl.value / 100;
-//high volume
-   if(music.volume > 0.75){
-    highVolumeIcon.style.display = 'block';
-    mediumVolumeIcon.style.display = 'none';
-    lowVolumeIcon.style.display = 'none';
-    zeroVolumeIcon.style.display = 'none';
-   }
-   //medium volume
-   if(music.volume >= 0.5 && music.volume < 0.75 || music.volume <= 0.5 && music.volume >= 0.25){
-    highVolumeIcon.style.display = 'none';
-    mediumVolumeIcon.style.display = 'block';
-    lowVolumeIcon.style.display = 'none';
-    zeroVolumeIcon.style.display = 'none';
-   }
-   //low volume
-   if(music.volume < 0.25){
-    highVolumeIcon.style.display = 'none';
-    mediumVolumeIcon.style.display = 'none';
-    lowVolumeIcon.style.display = 'block';
-    zeroVolumeIcon.style.display = 'none';
-   }
-   //zero volume
-   if(music.volume == 0){
-    highVolumeIcon.style.display = 'none';
-    mediumVolumeIcon.style.display = 'none';
-    lowVolumeIcon.style.display = 'none';
-    zeroVolumeIcon.style.display = 'block';
-   }
+    music.volume = volumeControl.value / 100;
+    //high volume
+    if (music.volume > 0.75) {
+        highVolumeIcon.style.display = 'block';
+        mediumVolumeIcon.style.display = 'none';
+        lowVolumeIcon.style.display = 'none';
+        zeroVolumeIcon.style.display = 'none';
+    }
+    //medium volume
+    if (music.volume >= 0.5 && music.volume < 0.75 || music.volume <= 0.5 && music.volume >= 0.25) {
+        highVolumeIcon.style.display = 'none';
+        mediumVolumeIcon.style.display = 'block';
+        lowVolumeIcon.style.display = 'none';
+        zeroVolumeIcon.style.display = 'none';
+    }
+    //low volume
+    if (music.volume < 0.25) {
+        highVolumeIcon.style.display = 'none';
+        mediumVolumeIcon.style.display = 'none';
+        lowVolumeIcon.style.display = 'block';
+        zeroVolumeIcon.style.display = 'none';
+    }
+    //zero volume
+    if (music.volume == 0) {
+        highVolumeIcon.style.display = 'none';
+        mediumVolumeIcon.style.display = 'none';
+        lowVolumeIcon.style.display = 'none';
+        zeroVolumeIcon.style.display = 'block';
+    }
 }
 // função para tocar a próxima música quando a atual chegar ao fim.
-function playNext(){
+function playNext() {
     indexMusic++;
-    if(indexMusic >= objectMusic.length){
+    if (indexMusic >= objectMusic.length) {
         indexMusic = 0;
     }
     renderMusic(indexMusic);
 }
 
 /////////////////////////////////
+
+//// declarações
+const btnListMusic = document.getElementById('btn-listMusic');
+const btnCloseMusic = document.getElementById('btn-closeMusic');
+const divGenderMusic = document.getElementById('gender-music');
+const btnGender = document.querySelector('.gender-option');
+const mainContainer = document.querySelector('.main-container');
+const divListBox = document.getElementById('list-box');
+/////Eventos
+//// Clique em list music para selecionar músicas
+btnListMusic.addEventListener('click', function () {
+    divGenderMusic.innerHTML = '';
+    btnListMusic.style.display = 'none';
+    btnCloseMusic.style.display = 'block'
+    mainContainer.style.display = 'none';
+    divGenderMusic.style.display = 'block';
+    let btnAllMusics = document.createElement('button');
+    btnAllMusics.classList.add('gender-option');
+    btnAllMusics.textContent = 'All Musics';
+    divGenderMusic.appendChild(btnAllMusics);
+    listOfGenders();
+})
+///Clique em close list para retornar ao normal
+btnCloseMusic.addEventListener('click', function () {
+    btnCloseMusic.style.display = 'none';
+    btnListMusic.style.display = 'block';
+    divGenderMusic.style.display = 'none';
+    divListBox.style.display = 'none';
+    mainContainer.style.display = 'block';
+})
+
+function listOfGenders(){
+
+    let uniqueGenders = [];
+    objectMusic.forEach(genders => {
+        if(genders.gender && !uniqueGenders.includes(genders.gender)){
+            uniqueGenders.push(genders.gender);
+        }
+    });
+    uniqueGenders.forEach(gender =>{
+            let btnOptionGender = document.createElement('button');
+            btnOptionGender.classList.add('gender-option');
+            btnOptionGender.textContent = gender;
+            divGenderMusic.appendChild(btnOptionGender)
+    })
+}
+
+function playdMusicsByGender(){
+
+}
