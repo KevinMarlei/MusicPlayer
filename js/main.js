73661482d012f66a1bda8c4777fbd9c123bd319a
Kevin Marlei <kevin_SSDP@hotmail.com>
2023-12-17 14:53:26 +0100

    Atualização de todo o projeto

--- js/main.js
+++ js/main.js
@@ -1,193 +1,244 @@
-//declarações
+//declarções 
 let music = document.querySelector('audio');
-let indexMusic = 0;
+let imageMusic = document.querySelector('img');
+let title = document.querySelector('.title');
+let artist = document.querySelector('.artist-name');
 let musicDuration = document.querySelector('.end');
-let image = document.querySelector('img');
-let musicName = document.querySelector('.description h2');
-let artistName = document.querySelector('.description i');
-let volumeControl = document.querySelector('.volume');
+const btnPlay = document.querySelector('.btn-play');
+const btnPrevious = document.querySelector('.btn-previous');
+const btnPause = document.querySelector('.btn-pause');
+const btnNext = document.querySelector('.btn-next');
+let progressBar = document.querySelector('.progress-bar');
+let currentTime = document.querySelector('.start');
+let volumeInput = document.querySelector('.volume');
 
-renderMusic(indexMusic);
+let indexMusic = 0;
+let genderChoice;
+let objectFiltered = objectMusic.filter((newObject) => newObject.gender === genderChoice);
+
 //eventos
-document.querySelector('.btn-play').addEventListener('click', playMusic);
-document.querySelector('.btn-pause').addEventListener('click', pauseMusic);
-volumeControl.addEventListener('input', controlVolume);
+btnPlay.addEventListener('click', playMusic);
+btnPause.addEventListener('click', pauseMusic);
+btnNext.addEventListener('click', ()=>{
+    indexMusic++;
+    if(objectFiltered && objectFiltered.length > 0){
+        if(indexMusic >= objectFiltered.length){
+            indexMusic = 0;
+        }
+    }else{
+        if(indexMusic >= objectMusic.length){
+            indexMusic = 0;
+        }
+    }
+    renderMusic(indexMusic);
+});
+btnPrevious.addEventListener('click', ()=>{
+    indexMusic--;
+    if(objectFiltered && objectFiltered.length > 0){
+        if(indexMusic < 0){
+            indexMusic = objectFiltered.length - 1; 
+        }
+    }else{
+        if(indexMusic < 0){
+            indexMusic = objectMusic.length - 1;
+        }
+    }
+    renderMusic(indexMusic);
+});
 
-volumeControl.addEventListener('wheel', function (event) {
-    event.preventDefault();
-    volumeControl.value = parseInt(volumeControl.value) + (event.deltaY > 0 ? -1 : 1);
-    volumeControl.value = Math.max(0, Math.min(100, volumeControl.value));
+volumeInput.addEventListener('input', controlVolume);
+volumeInput.addEventListener('wheel', function(event){
+    volumeInput.value = parseInt(volumeInput.value) + (event.deltaY > 0 ? -1 : 1);
+    volumeInput.value = Math.max(0, Math.min(100, volumeInput.value));
     controlVolume();
 });
-
-document.querySelector('.btn-next').addEventListener('click', () => {
-    indexMusic++;
-    if (indexMusic >= objectMusic.length) {
-        indexMusic = 0;
-    }else if(indexMusic != objectMusic){
-        
-    }
-    renderMusic(indexMusic);
-});
-document.querySelector('.btn-previous').addEventListener('click', () => {
-    indexMusic--;
-    if (indexMusic < 0) {
-        indexMusic = objectMusic.length - 1;
-    }
-    renderMusic(indexMusic);
-});
-
-music.addEventListener('timeupdate', updateBar)
-music.addEventListener('ended', playNext);
 //funções
 function playMusic() {
     music.play();
 }
 
-function pauseMusic() {
-
+function pauseMusic(){
     music.pause();
 }
 
-function updateBar() {
-    let range = document.querySelector('.progre[type="range"]');
-    let theCurrentTime = document.querySelector('.start');
+function playNext(){
+    indexMusic++;
+    if(objectFiltered && objectFiltered.length > 0){
+        if(indexMusic >= objectFiltered.length){
+            indexMusic = 0
+        }
+    }else{
+        if(indexMusic >= objectMusic.length){
+            indexMusic = 0;
+        }
+    }
+    renderMusic(indexMusic);
+}
+music.addEventListener('ended', playNext);
 
-    let percentage = (music.currentTime / music.duration) * 100;
-
-    range.value = percentage;
-    theCurrentTime.textContent = secondsToMinutes(Math.floor(music.currentTime));
+function renderMusic(index) {
+    if (objectFiltered.length > 0 && index < objectFiltered.length) {
+        music.setAttribute('src', objectFiltered[index].src);
+        music.addEventListener('loadeddata', () => {
+            title.textContent = objectFiltered[index].title;
+            artist.textContent = objectFiltered[index].artist;
+            imageMusic.src = objectFiltered[index].img;
+            musicDuration.textContent = secondsToMinutes(Math.floor(music.duration));
+            console.log(music.duration);
+        });
+    } else {
+        music.setAttribute('src', objectMusic[index].src);
+        music.addEventListener('loadeddata', () => {
+            title.textContent = objectMusic[index].title;
+            artist.textContent = objectMusic[index].artist;
+            imageMusic.src = objectMusic[index].img;
+            musicDuration.textContent = secondsToMinutes(Math.floor(music.duration));
+            console.log(music.duration);
+        });
+    }
+    playMusic();
 }
 
-function clickBar() {
-    let range = document.querySelector('.progre[type="range"]');
-    let music = document.querySelector('audio');
-
-    range.addEventListener('input', function () {
-        // Obtém a porcentagem do controle deslizante
-        let percentage = range.value;
-
-        // Calcula o novo tempo da música com base na porcentagem
-        let newTime = (percentage / 100) * music.duration;
-
-        // Define o tempo da música
-        music.currentTime = newTime;
-
-        // Atualiza a barra de progresso
-        updateBar();
-    });
-}
-clickBar();
-
-
-function secondsToMinutes(seconds) {
+function secondsToMinutes(seconds){
     let fieldMinutes = Math.floor(seconds / 60);
     let fieldSeconds = seconds % 60;
-    if (fieldSeconds < 10) {
+    if(fieldSeconds < 10){
         fieldSeconds = '0' + fieldSeconds;
     }
     return `${fieldMinutes}:${fieldSeconds}`;
 }
 
+function updateBar(){
+    let percent = (music.currentTime / music.duration) * 100;
+    progressBar.value = percent;
+    currentTime.textContent = secondsToMinutes(Math.floor(music.currentTime));
+}
+music.addEventListener('timeupdate', updateBar);
 
-function renderMusic(index) {
-        music.setAttribute('src', objectMusic[index].src);
-        music.addEventListener('loadeddata', () => {
-            musicName.textContent = objectMusic[index].title;
-            artistName.textContent = objectMusic[index].artist;
-            image.src = objectMusic[index].img;
-            musicDuration.textContent = secondsToMinutes(Math.floor(music.duration));
-        });
-    playMusic();
+function clickBar(){
+    progressBar.addEventListener('input', ()=>{
+        let percent = progressBar.value;
+        let newTime = (percent / 100) * music.duration;
+        music.currentTime = newTime;
+    });
+    updateBar();
 }
 
-function controlVolume() {
-    const highVolumeIcon = document.querySelector('.high-volume');
-    const mediumVolumeIcon = document.querySelector('.medium-volume');
-    const lowVolumeIcon = document.querySelector('.low-volume');
-    const zeroVolumeIcon = document.querySelector('.zero-volume');
-    music.volume = volumeControl.value / 100;
-    //high volume
-    if (music.volume > 0.75) {
-        highVolumeIcon.style.display = 'block';
-        mediumVolumeIcon.style.display = 'none';
-        lowVolumeIcon.style.display = 'none';
-        zeroVolumeIcon.style.display = 'none';
+function controlVolume(){
+    const highVolume = document.querySelector('.high-volume');
+    const mediumVolume = document.querySelector('.medium-volume');
+    const lowVolume = document.querySelector('.low-volume');
+    const zeroVolume = document.querySelector('.zero-volume');
+
+    music.volume = volumeInput.value / 100
+    if(music.volume > 0.75){
+        highVolume.style.display = 'block';
+        mediumVolume.style.display = 'none';
+        lowVolume.style.display = 'none';
+        zeroVolume.style.display = 'none';
     }
-    //medium volume
-    if (music.volume >= 0.5 && music.volume < 0.75 || music.volume <= 0.5 && music.volume >= 0.25) {
-        highVolumeIcon.style.display = 'none';
-        mediumVolumeIcon.style.display = 'block';
-        lowVolumeIcon.style.display = 'none';
-        zeroVolumeIcon.style.display = 'none';
+    if(music.volume >= 0.5 && music.volume < 0.75 || music.volume <= 0.5 && music.volume >= 0.25){
+        highVolume.style.display = 'none';
+        mediumVolume.style.display = 'block';
+        lowVolume.style.display = 'none';
+        zeroVolume.style.display = 'none';
     }
-    //low volume
-    if (music.volume < 0.25) {
-        highVolumeIcon.style.display = 'none';
-        mediumVolumeIcon.style.display = 'none';
-        lowVolumeIcon.style.display = 'block';
-        zeroVolumeIcon.style.display = 'none';
+    if(music.volume < 0.25){
+        highVolume.style.display = 'none';
+        mediumVolume.style.display = 'none';
+        lowVolume.style.display = 'block';
+        zeroVolume.style.display = 'none';
     }
-    //zero volume
-    if (music.volume == 0) {
-        highVolumeIcon.style.display = 'none';
-        mediumVolumeIcon.style.display = 'none';
-        lowVolumeIcon.style.display = 'none';
-        zeroVolumeIcon.style.display = 'block';
+    if(music.volume === 0){
+        highVolume.style.display = 'none';
+        mediumVolume.style.display = 'none';
+        lowVolume.style.display = 'none';
+        zeroVolume.style.display = 'block';
     }
 }
-// função para tocar a próxima música quando a atual chegar ao fim.
-function playNext() {
-    indexMusic++;
-    if (indexMusic >= objectMusic.length) {
+
+clickBar();
+renderMusic(indexMusic);
+
+//////////////////////////////////////////////////////
+
+const btnListMusic = document.querySelector('.btn-list-music');
+const btnCloseList = document.querySelector('.btn-close-list');
+const divListBox = document.getElementById('list-box');
+const divMusicalGender = document.querySelector('.musical-gender');
+const lowBoxList = document.querySelector('.low-box-list');
+const btnGender = document.querySelectorAll('.btn-gender');
+const divBoxImage = document.getElementById('box-image');
+
+btnListMusic.addEventListener('click', ()=>{
+    btnListMusic.style.display = 'none';
+    btnCloseList.style.display = 'flex';
+    divListBox.style.display = 'flex';
+    divMusicalGender.style.display = 'flex';
+    divBoxImage.style.display = 'none';
+
+    document.querySelectorAll('.btn-gender:not(:first-child)').forEach(btn => btn.remove());
+
+    const uniqueGenders = [];
+    objectMusic.forEach((allGender)=>{
+        const gender = allGender.gender;
+        if(!uniqueGenders.includes(gender)){
+            uniqueGenders.push(gender);
+            btnGender.forEach(()=>{
+                const button = document.createElement('button');
+                button.classList.add('btn-gender');
+                button.textContent = gender;
+                lowBoxList.appendChild(button);
+
+                button.addEventListener('click', ()=>{
+                    genderOrAllMusicCheck(button);
+                });
+            });
+        }
+    });
+});
+
+btnCloseList.addEventListener('click', ()=>{
+    btnCloseList.style.display = 'none';
+    btnListMusic.style.display = 'flex';
+    divListBox.style.display = 'none';
+    divMusicalGender.style.display = 'none';
+    divBoxImage.style.display = 'block';
+});
+
+btnGender.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        if(btn.textContent === 'All Musics'){
+            genderChoice = null;
+            objectFiltered = objectMusic;
+        }else{
+            genderChoice = btn.textContent;
+            objectFiltered = objectMusic.filter((newObject) => newObject.gender === genderChoice);
+        }
+        btnCloseList.style.display = 'none';
+        btnListMusic.style.display = 'flex';
+        divListBox.style.display = 'none';
+        divMusicalGender.style.display = 'none';
+        divBoxImage.style.display = 'block';
         indexMusic = 0;
+        renderMusic(indexMusic);
+    });
+});
+
+function genderOrAllMusicCheck(button){
+    btnCloseList.style.display = 'none';
+    btnListMusic.style.display = 'flex';
+    divListBox.style.display = 'none';
+    divMusicalGender.style.display = 'none';
+    divBoxImage.style.display = 'block';
+
+    if(button.textContent === 'All Musics'){
+        genderChoice = null;
+        objectFiltered = objectMusic;
+    }else{
+        genderChoice = button.textContent;
+        objectFiltered = objectMusic.filter((newObject) => newObject.gender === genderChoice);
     }
+    indexMusic = 0
     renderMusic(indexMusic);
 }
-
-/////////////////////////////////
-
-//// declarações
-const btnListMusic = document.getElementById('btn-listMusic');
-const btnCloseMusic = document.getElementById('btn-closeMusic');
-const mainContainer = document.querySelector('.main-container');
-const divListBox = document.getElementById('list-box');
-const chosenList = document.querySelector('.chosen-list');
-/////Eventos
-//// Clique em list music para selecionar músicas
-btnListMusic.addEventListener('click', function () {
-    btnListMusic.style.display = 'none';
-    btnCloseMusic.style.display = 'block'
-    mainContainer.style.display = 'none';
-    divListBox.style.display = 'block';
-    chosenList.innerHTML = '';
-
-    //itera sobre o objeto criando um button pra cada title diferente que está no array
-    objectMusic.forEach(function (musics, index){
-        let btnMusics = document.createElement('button');
-        btnMusics.classList.add('music-button');
-        btnMusics.textContent = musics.title
-
-        //ao clicar em alguma música que estão como buttons, chama a renderMusic e passa a ela como parâmetro a posição da música selecionada que está contido no array original. Entretanto isso não faz com que o aplicativo dê sequência apartir da música selecionada quando ela acaba. O array volta para a posição da música anterior e prosegue apartir daí. Exemplo: Se o array estava na posição 1 e a música da posição 8 é selecionada, ao invés de continuar da música da posição 9 quando a da posição 8 finalizar o seu tempo, ele volta para a posição 0 e da sequência para a seguinte da posição 1.
-        btnMusics.addEventListener('click', function(){
-            renderMusic(index);
-            btnListMusic.style.display = 'block';
-            btnCloseMusic.style.display = 'none'
-            mainContainer.style.display = 'block';
-            divListBox.style.display = 'none';
-        });
-        //cria elemento LI e depois passa a variável que cria os button como filhos da li, e depois a variável chosenList que contem o elemento ol recebe como filho a variável que contém todos os li.
-        let elementLi = document.createElement('li');
-        elementLi.appendChild(btnMusics);
-        chosenList.appendChild(elementLi);
-    })
-   
-})
-///Clique em close list para retornar ao normal
-btnCloseMusic.addEventListener('click', function () {
-    btnCloseMusic.style.display = 'none';
-    btnListMusic.style.display = 'block';
-    divListBox.style.display = 'none';
-    mainContainer.style.display = 'block';
-})
-
---
+++ js/objectMusic.js
@@ -0,0 +1,30 @@
+const objectMusic = [
+   {
+      gender: 'Country&Folk',
+      title: 'To Weather a Storm',
+      artist: 'Dan Lebowitz',
+      src: 'musics/To Weather a Storm - Dan Lebowitz.mp3',
+      img: 'img/country&folk.jpg'
+   },
+   {
+      gender: 'Jazz and Blues',
+      title: 'Eletrosamba',
+      artist: 'Quincas Moreira',
+      src: 'musics/Eletrosamba - Quincas Moreira.mp3',
+      img: 'img/jazzBlues.jpg'
+   },
+   {
+      gender: 'Country&Folk',
+      title: 'Dirty Mac',
+      artist: 'Endless Love',
+      src: 'musics/Dirty Mac - Endless Love.mp3',
+      img: 'img/country&folk.jpg'
+   },
+   {
+      gender: 'Rock',
+      title: 'Elevators Need Rock Too',
+      artist: 'Spence',
+      src: 'musics/Elevators Need Rock Too - Spence.mp3',
+      img: 'img/rock.jpg'
+   },
+]
