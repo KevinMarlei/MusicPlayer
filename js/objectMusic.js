dedb85d3d0ece713fd7c3b5931f3ae9edbe346d2
Kevin Marlei <kevin_SSDP@hotmail.com>
2024-08-02 16:03:54 +0200

    Adição de músicas e imagem para testes

--- js/objectMusic.js
+++ js/objectMusic.js
@@ -27,4 +27,130 @@ const objectMusic = [
       src: 'musics/Elevators Need Rock Too - Spence.mp3',
       img: 'img/rock.jpg'
    },
+   {
+      gender: 'My music',
+      title: 'Light Infection',
+      artist: 'Prague',
+      src: 'musics/Light Infection.mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Smells Like Team Spirit',
+      artist: 'Nirvana',
+      src: 'musics/Nirvana - Smells Like Teen Spirit.mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Come as You Are',
+      artist: 'Nirvana',
+      src: 'musics/Come as You Are - Nirvana.mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Chop Suey',
+      artist: 'System of a Down',
+      src: 'musics/System of a Down - Chop Suey (Remastered 2021).mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Toxicity',
+      artist: 'System of a Down',
+      src: 'musics/System Of A Down - Toxicity (Official HD Video).mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Kinetic',
+      artist: 'DJ Sona’s',
+      src: 'musics/DJ Sona’s Ultimate Skin Music Kinetic.mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Legends Never Die',
+      artist: 'League of Legend’s',
+      src: 'musics/Legends Never Die (ft. Against The Current) [OFFICIAL AUDIO]  Worlds 2017 - League of Legends.mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Gravity Wall',
+      artist: 'RE: Creator’s',
+      src: 'musics/Gravity Wall.mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Fly Me To The Moon',
+      artist: 'Frank Sinatra',
+      src: 'musics/Fly Me To The Moon (2008 Remastered).mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'A Cruel Angel’s Thesi’s',
+      artist: 'Frank Sinatra',
+      src: 'musics/Shiro Sagisu, Yoko Takahashi - A Cruel Angels Thesis - (Evangelion Original Soundtrack).mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Tremor',
+      artist: 'Dimitri Vegas, Martin Garrix, Like Mike',
+      src: 'musics/Dimitri Vegas, Martin Garrix, Like Mike - Tremor (Official Music Video).mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Innocence',
+      artist: 'NoisyCell',
+      src: 'musics/NoisyCell - Innocence ［OFFICIAL MUSIC VIDEO］.mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Pra Você',
+      artist: 'Onze:20',
+      src: 'musics/Onze20 - Pra Você (Áudio).mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'No Rancho Fundo',
+      artist: 'Xitãozinho e Xororó',
+      src: 'musics/Chitãozinho e Xororó   No Rancho Fundo.mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Fio de Cabelo',
+      artist: 'Xitãozinho e Xororó',
+      src: 'musics/Chitãozinho e Xororó - Fio De Cabelo.mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Creep',
+      artist: 'RadioHead',
+      src: 'musics/Creep.mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Stay With Me',
+      artist: 'Matsubara Miki',
+      src: 'musics/Miki Matsubara - Stay With Me HD (Club Mix).mp3',
+      img: 'img/cerejeiras.gif'
+   },
+   {
+      gender: 'My music',
+      title: 'Otherside',
+      artist: 'Red Hot Cjili Pepper’s',
+      src: 'musics/Red Hot Chili Peppers - Otherside (Lyrics).mp3',
+      img: 'img/cerejeiras.gif'
+   },
 ]
